40ff755b660b5caeba0f87b4dd5dbdbb059deae0
Vincent <gyurgyikv@gmail.com>
2023-07-02 12:54:00 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using RestSharp;
+
+namespace SidePrjoectAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please enter a word: ");
+            string word = Console.ReadLine();
+
+            RestClient client = new RestClient();
+            RestRequest request = new RestRequest($"https://api.dictionaryapi.dev/api/v2/entries/en/{word}");
+            RestResponse response = client.Get(request);
+            Console.WriteLine(response.Content);
+        }
+    }
+}
